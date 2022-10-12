2177312af6334b98097298c3d9c9f44499ca790c
Kevin <kevin.j.moll@gmail.com>
2022-10-12 09:10:41 -0400

    updated path

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a617320bac8d14196817df5986e0fc90-307391793.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
